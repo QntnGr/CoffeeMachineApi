cd358455895469580f49fbe065467b95013ae098
Quentin Goeury <qgoeury@gmail.com>
2024-06-23 13:25:54 +0200

    entities setup

---
+++ CoffeeMachineApi/Entities/Drink.cs
@@ -0,0 +1,10 @@
+﻿namespace CoffeeMachineApi.Entities
+{
+    public class Drink
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Ingredient> Ingredients { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
---
+++ CoffeeMachineApi/Entities/Ingredient.cs
@@ -0,0 +1,9 @@
+﻿namespace CoffeeMachineApi.Entities;
+
+public class Ingredient
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal UnitPrice { get; set; }
+    public int Count { get; set; }
+}
